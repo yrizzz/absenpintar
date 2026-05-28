c38bd5e89eb83a9a74f0637fd700f2c3801db97a
yrizzz <rekberariez@gmail.com>
2026-05-28 15:23:56 +0700

    fix: register spatie permission middlewares in bootstrap/app.php

--- pulse-presence/bootstrap/app.php
+++ pulse-presence/bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->trustProxies(at: '*');
         $middleware->alias([
             'active' => \App\Http\Middleware\EnsureUserIsActive::class,
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
